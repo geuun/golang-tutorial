ac3bc9966de2a8247ed365c05a4956ae7f4c5aa5
Geuun <jgk9612@gmail.com>
2025-01-15 23:37:46 +0900

    feat: go example 추가

---
+++ go-by-example/pointer.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func zeroval(ival int) {
+	ival = 0
+}
+
+func zeroptr(iptr *int) {
+	*iptr = 0
+}
+
+func main() {
+	i := 1
+	fmt.Println("initial:", i)
+
+	zeroval(i)
+	fmt.Println("zeroval:", i)
+
+	zeroptr(&i)
+	fmt.Println("zeroptr:", i)
+
+	fmt.Println("pointer:", &i)
+
+	var a int = 10
+	var b int = 20
+
+	var intPointer1 *int = &a
+	var intPointer2 *int = &b
+
+	fmt.Println("a:", intPointer1)
+	fmt.Println("b:", intPointer2)
+}
